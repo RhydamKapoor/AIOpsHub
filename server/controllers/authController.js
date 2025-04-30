477d5961980955dfcb221eb683cad59b2a9a9c89
Rhydam Kapoor <rhydamkapoor@gmail.com>
2025-04-30 12:07:55 +0530

    Render testing #2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -37,7 +37,7 @@ exports.login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
       path: '/'
     });
